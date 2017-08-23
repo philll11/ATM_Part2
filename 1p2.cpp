0df2fef05a62d7c0afd168a73b36f9854a2d00b9
Leonard Phillips <philll11@users.noreply.github.com>
2017-08-23 22:18:57 +1200

    Final Documentation
    This is the finished programming

--- 1p2.cpp
+++ 1p2.cpp
@@ -1,13 +1,14 @@
-/*
-Leonard Phillips
-15232331
-Assignment 1 Part 2
-This is an ATM. 
-It takes input from two files (acct.txt, tranz.txt).
-It then applied changes to the information recieved from acct.txt.
-These changes depend on the instructions read in from tranz.txt.
-Instructions that include depositing and withdrawing.
-*/
+/**
+  * Leonard Phillips
+  * 15232331
+  * Assignment 1 Part 2
+  * This is an ATM. 
+  * It takes input from two files (acct.txt, tranz.txt).
+  * It then applied changes to the information recieved from acct.txt.
+  * These changes depend on the instructions read in from tranz.txt.
+  * Instructions that include depositing and withdrawing.
+  * It was built using Object Orientated Programming design.
+  */
 
 #include <iostream>
 #include <fstream>
@@ -15,64 +16,88 @@ Instructions that include depositing and withdrawing.
 
 using namespace std;
 
-//=================================================================================
-
 const int MAX_ACC_SIZE = 100;
 const int MAX_TRAN_SIZE = 100;
 
-
+//=================================================================================
+/**
+ * A class representing a transaction
+ */
 class Transaction {
 	public:
-		Transaction(int newId = 0, char newType = 'A', double newAmount = 0);	
-		friend ostream& operator<<(ostream& output, const Transaction& a);
+		/**
+		 *	Constructor
+		 * @param[in] newId 		A account identifier.
+		 * @param[in] newType	Type of transaction
+		 * @param[in] newAmount	Value of transaction		
+		 */
+		Transaction(int newId = 0, char newType = 'A', double newAmount = 0);
+		/**
+		 *	mId Accessor
+		 * Returns mId
+		 */
 		int getId() { return mId; }
+		/**
+		 *	mType Accessor
+		 * Returns mType
+		 */
 		char getType() { return mType; }
+		/**
+		 *	mAmount Accessor
+		 * Returns mAmount
+		 */
 		double getAmount() { return mAmount; }
 	private:
-		int mId;
-		char mType;
-		double mAmount;
+		int mId; ///< The ID of the account the transaction will operate on
+		char mType; ///< The type of transaction to be preformed
+		double mAmount; ///< The amount by which the account balance will be altered
 };
-
 //---------------------------------------------------------------
-
 Transaction::Transaction(int newId, char newType, double newAmount) : 
 		mId(newId), mType(newType), mAmount(newAmount) {}
-
 //---------------------------------------------------------------
-
-ostream& operator<<(ostream& output, const Transaction& a) {
-	output << "Acct#\t" << a.mId << "\tBal:\t" 
-		<< setprecision(2) << fixed << a.mAmount
-		<< "    \tTrans so far:\t" << a.mType << endl;
-	
-	return output;
-}
-
-//---------------------------------------------------------------
-
 //=================================================================================
-
-
+/**
+ * A class representing an account
+ */
 class Account {
 	public:
+		/**
+		 * Constructor
+		 * @param[in] mId			The account identifier
+		 * @param[in] mBalance	The initile value of account
+		 */
 		Account(int mId = 0, double mBalance = 0);
+		/**
+		 * Member Function applyTrans
+		 * @param[in] t[]			An array of type transaction containing data that will mutate mBalance data member
+		 * @param[in] transSize	The length of t array
+		 */
 		void applyTrans(Transaction t[MAX_TRAN_SIZE], int transSize);
+		/**
+		 * Member Function withdraw
+		 * @param[in] amount	The account to be withdrawn from mBalance data member
+		 */
 		void withdraw(double& amount);
+		/**
+		 * Member Function deposit
+		 * @param[in] amount	The account to be deposited into mBalance data member
+		 */
 		void deposit(double& amount);
+		/**
+		 *	Overloaded output operator
+		 * @param[in] output 	The stream to be returned containing the output
+		 * @param[in] newType	A constant reference to a Account object
+		 */
 		friend ostream& operator<<(ostream& output, const Account& a);
 	private:
-		int mId;
-		int mTrans;
-		double mBalance;
+		int mId; ///< The identifcation number of the account
+		int mTrans; ///< The number of transactions that have altered the account
+		double mBalance; ///< The amount of capital in the account
 };
-
 //---------------------------------------------------------------
-
 Account::Account(int newId, double newBal) : mId(newId), mTrans(0), mBalance(newBal) {}
-
 //---------------------------------------------------------------
-
 void Account::applyTrans(Transaction t[MAX_TRAN_SIZE], int transSize) {
 	for(int i = 0; i < transSize; ++i){
 		if(mId == t[i].getId()){
@@ -85,26 +110,20 @@ void Account::applyTrans(Transaction t[MAX_TRAN_SIZE], int transSize) {
 			}
 		}
 	}
-}
-	
+}	
 //---------------------------------------------------------------
-
 void Account::withdraw(double& amount) {
 	if((mBalance - amount) > 0) {
 		mBalance -= amount;
 		++mTrans;
 	}
 }
-
 //---------------------------------------------------------------
-
 void Account::deposit(double& amount) {
 	mBalance += amount;
 	++mTrans;
 }
-
 //---------------------------------------------------------------
-
 ostream& operator<<(ostream& output, const Account& a) {
 	output << "Acct#\t" << a.mId << "\tBal:\t" 
 		<< setprecision(2) << fixed << a.mBalance
@@ -112,35 +131,36 @@ ostream& operator<<(ostream& output, const Account& a) {
 	
 	return output;
 }
-
 //---------------------------------------------------------------
-
 //=================================================================================
-
-
-int fileCheck(ifstream &ifstr, string f);
 /**
 	*Checks the file exsits. If the file doesn't exist then a string 
 	*is printing on the screen informing the user.
-	*@param[in]: the file stream.
-	*@param[in]: the the name of a file being passed as a string type.
-**/	
+	*@param[in] ifstr The file stream.
+	*@param[in] f		The name of a file being checked. Is used to identify what file is missing.
+	*/
+int fileCheck(ifstream &ifstr, string f);
 
+/**
+	* Global Function readAccsInfo.
+	* Assigns data from the file stream to the Tccount objects in a[].
+	* @param[in] ifs	The file stream.
+	* @param[in] a[] 	The Account array that will sort the account data
+	*/
 int readAccsInfo(ifstream& ifs, Account a[MAX_ACC_SIZE]);
+/**
+	* Global Function readTransInfo.
+	* Assigns data from the file stream to the Transaction objects in t[].
+	* @param[in] ifs	The file stream.
+	* @param[in] t[]	The Transaction array that wil sort the transaction data.
+	*/
 int readTransInfo(ifstream& ifs, Transaction t[MAX_TRAN_SIZE]);
 /**
-	* Reads Account details from the "accs.txt" file. It then creates a new Account object 
-	* using each line from the file. The new Account object then added to the Account array a[100].
-	* @param[in]: the file stream
-	* @param[in, out]: the default array of Account being mutated
-**/
+   * Prints developers credentials
+   */
 void print(){
 	cout << "Assignment 1\t\tpart 1\n" << "Leonard Phillips\t15232331\n" << "159.234\t\t\tOOP\n\n";
 }
-/**
-  *Prints developers credentials
-*/
-
 //=================================================================================
 
 int main() {
@@ -194,7 +214,6 @@ int main() {
 }
 
 //=================================================================================
-
 //---------------------------------------------------------------
 int fileCheck(ifstream &ifstr, string f)
 {
